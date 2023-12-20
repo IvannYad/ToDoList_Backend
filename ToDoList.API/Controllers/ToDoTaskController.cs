64a3434db1ae0cded2485129a13c8b5044e1afcf
Ivan Yadchyshyn <yad7devadress@gmail.com>
2023-12-20 16:18:04 +0200

    feat: add ToDoTaskController and implement its constructor

---
+++ ToDoList.API/Controllers/ToDoTaskController.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using ToDoList.BLL.GeneralTypes;
+using ToDoList.DAL.Repository.Interfaces;
+
+namespace ToDoList.API.Controllers
+{
+    [ApiController]
+    [Route("api/Task")]
+    public class ToDoTaskController : ControllerBase
+    {
+        protected APIResponse _response;
+        private readonly IToDoTaskRepository _toDoTaskRepository;
+        private readonly IMapper _mapper;
+        public ToDoTaskController(IToDoTaskRepository toDoTaskRepository, IMapper mapper)
+        {
+            _toDoTaskRepository = toDoTaskRepository;
+            _mapper = mapper;
+            _response = new();
+        }
+    }
+}
