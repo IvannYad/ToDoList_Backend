98f75fffae891af05e7b19d324e628c0ee12485b
Ivan Yadchyshyn <yad7devadress@gmail.com>
2023-12-20 18:19:29 +0200

    feat: extend APIResponse with methods to generate APIResponse with different responses

--- ToDoList.BLL/GeneralTypes/APIResponse.cs
+++ ToDoList.BLL/GeneralTypes/APIResponse.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -13,5 +14,26 @@ namespace ToDoList.BLL.GeneralTypes
         public bool IsSuccess { get; set; } = true;
         public List<string>? ErrorMessages { get; set; }
         public object? Result { get; set; }
+
+        public void Generate200OK(object? data)
+            => GenerateResponse(true, null, HttpStatusCode.OK, data);
+
+        public void Generate400BadRequest(List<string>? errorMessages)
+            => GenerateResponse(false, errorMessages, HttpStatusCode.BadRequest, null);
+
+        public void Generate404NotFound(List<string>? errorMessages)
+            => GenerateResponse(false, errorMessages, HttpStatusCode.NotFound, null);
+
+
+        private void GenerateResponse(bool isSuccess
+            , List<string>? errorMessages
+            , HttpStatusCode statusCode
+            , object? data)
+        {
+            this.IsSuccess = isSuccess;
+            this.ErrorMessages = errorMessages;
+            this.StatusCode = statusCode;
+            this.Result = data;
+        }
     }
 }
