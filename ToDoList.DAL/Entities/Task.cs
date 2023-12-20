f7998ac7d2f3bb5e04e2834950dde1b86f444fe0
Ivan Yadchyshyn <yad7devadress@gmail.com>
2023-12-20 09:06:50 +0200

    feat: implement Task entity

--- ToDoList.DAL/Entities/Task.cs
+++ ToDoList.DAL/Entities/Task.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +11,28 @@ namespace ToDoList.DAL.Entities
 {
     internal class Task
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [MinLength(5)]
+        [MaxLength(50)]
+        public string TaskTitle { get; set; }
+
+        [Required]
+        [DisplayName("Description")]
+        [MaxLength(80)]
+        public string AdditionalDescription { get; set; }
+
+        [Required]
+        public DateTime TaskStartTime { get; set; }
+
+        [Required]
+        [DisplayName("Finish time")]
+        public DateTime TaskEndTime { get; set; }
+
+        [Required]
+        public int Status { get; set; }
     }
 }
--- ToDoList.DAL/Repository/Interfaces/ITaskRepository.cs
+++ ToDoList.DAL/Repository/Interfaces/ITaskRepository.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace ToDoList.DAL.Repository.Interfaces
 {
-    internal interface ITaskRepository
+    public interface ITaskRepository
     {
     }
 }
